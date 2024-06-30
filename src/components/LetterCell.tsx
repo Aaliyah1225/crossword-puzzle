5cdf796b2e43b1439dc0f5956a17089091f00d34
Aaliyah Haynes <165962589+Geeky99@users.noreply.github.com>
2024-06-30 14:54:28 -0400

    New changes

---
+++ src/components/LetterCell.tsx
@@ -0,0 +1,15 @@
+<html lang="en">
+
+<head>
+  <meta charSet="UTF-8" />
+  <title>Crossword Puzzle Game</title>
+  <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+<div className="crossword-puzzle">
+      <div className="cell">
+      </div>
+</div>
+</body>
+</html>
