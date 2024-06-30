46db1c080e477ea73cec10f5dc902870654fe44b
Aaliyah Haynes <165962589+Geeky99@users.noreply.github.com>
2024-06-30 14:26:24 -0400

    New Change

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,25 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
-  )
+<html lang="en">
+
+<head>
+  <meta charSet="UTF-8" />
+  <title>Crossword Puzzle Game</title>
+  <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+<div className="crossword-puzzle">
+      <div className="cell blank"></div>
+      <div className="cell filled">     
+        <span className="number">12</span>
+        <div className="letter">X</div>
+    </div>
+</div>
+</body>
+</html>
+  );
 }
 
-export default App
+export default App;
