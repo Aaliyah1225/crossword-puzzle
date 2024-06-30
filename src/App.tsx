7c0f9ce9f5e1ad50929496cd4ef695ed8f2bfb4d
Aaliyah Haynes <165962589+Geeky99@users.noreply.github.com>
2024-06-30 15:44:17 -0400

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
+import LetterCell from './components/LetterCell'
 import './App.css'
 
 function App() {
   return (
-<html lang="en">
-
-<head>
-  <meta charSet="UTF-8" />
-  <title>Crossword Puzzle Game</title>
-  <link rel="stylesheet" href="styles.css" />
-</head>
-
-<body>
-<div className="crossword-puzzle">
-      <div className="cell blank"></div>
-      <div className="cell filled">     
-        <span className="number">12</span>
-        <div className="letter">X</div>
-    </div>
-</div>
-</body>
-</html>
+<main> 
+  <LetterCell />
+</main>
   );
 }
 
--- src/components/LetterCell.tsx
+++ src/components/LetterCell.tsx
@@ -1,23 +1,9 @@
-import '/.LetterCell.css'
+import './LetterCell.css'
 
 function LetterCell() {
   return (
     <>
-<html lang="en">
-
-<head>
-  <meta charSet="UTF-8" />
-  <title>Crossword Puzzle Game</title>
-  <link rel="stylesheet" href="styles.css" />
-</head>
-
-<body>
-<div className="crossword-puzzle">
-      <div className="cell">
-      </div>
-</div>
-</body>
-</html>
+<h1>Hello World</h1>
 </>
   );
 }
