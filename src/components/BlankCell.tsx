637eb9adc1412bba2c8358834e262f27e86e2f68
Aaliyah Haynes <165962589+Geeky99@users.noreply.github.com>
2024-07-22 15:55:27 -0400

    New Changes

--- src/components/BlankCell.tsx
+++ src/components/BlankCell.tsx
@@ -1,10 +1,10 @@
-import styles from "./BlankCell.module.css"
-import cellStyles from "./Cell.module.css"
+import styles from "./BlankCell.module.css";
+import cellStyles from "./Cell.module.css";
 function BlankCell() {
-    return (
-<>
-<div className={`${styles.blank} ${cellStyles.cell}`}></div>
-</>
-    );
+  return (
+    <>
+      <div className={`${styles.blank} ${cellStyles.cell}`}></div>
+    </>
+  );
 }
 export default BlankCell;
