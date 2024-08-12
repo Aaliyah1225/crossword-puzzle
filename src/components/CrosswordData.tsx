57c918d5e15519b66bf4cf086683e1a8399c76bc
Aaliyah Haynes <165962589+Geeky99@users.noreply.github.com>
2024-08-12 16:16:23 -0400

    New Changes

---
+++ src/components/CrosswordData.tsx
@@ -0,0 +1,23 @@
+// Readonly Interface
+interface CrosswordData {
+  readonly playerAnswer: readonly string[][];
+  readonly correctAnswer: readonly string[][];
+}
+
+//Example Objects
+let data: CrosswordData = {
+  playerAnswer: [
+    ["_", "_", "_", "_", ".", "."],
+    ["_", "_", "_", "_", "_", "."],
+    [".", "_", "_", "_", "_", "_"],
+    [".", ".", "_", "_", "_", "_"],
+  ],
+  correctAnswer: [
+    ["A", "H", "E", "M", ".", "."],
+    ["C", "O", "D", "E", "R", "."],
+    [".", "P", "E", "T", "E", "R"],
+    [".", ".", "N", "S", "F", "W"],
+  ],
+};
+
+console.log(data)
