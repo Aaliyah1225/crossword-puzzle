cf973755c334e981a1aae3cf5274e8ab5c949ddd
Aaliyah <aaliyah_haynes@outlook.com>
2024-12-27 16:11:38 -0500

    New Changes

--- src/components/CrosswordGrid.tsx
+++ src/components/CrosswordGrid.tsx
@@ -3,37 +3,38 @@ import LetterCell from "./LetterCell";
 import "./CrosswordGrid.css";
 import { data } from "./CrosswordData.ts";
 export default function CrosswordGrid() {
-  const dataAnswer = data.playerAnswer.map((answer) => 
+  
+  const dataAnswer = data.playerAnswer[0].map((answer) =>
     <div>{answer}</div>
   );
 
   return (
     <>
       <div className="crossword">
-        <LetterCell letter={dataAnswer} number="1" />
-        <LetterCell number="2" />
-        <LetterCell number="3" />
-        <LetterCell number="4" />
+        <LetterCell letter={dataAnswer[0]} number="1" />
+        <LetterCell letter={dataAnswer[0]} number="2" />
+        <LetterCell letter={dataAnswer[0]} number="3" />
+        <LetterCell letter={dataAnswer[0]} number="4" />
         <BlankCell />
         <BlankCell />
-        <LetterCell letter="H" number="5" />
-        <LetterCell letter="E" />
-        <LetterCell letter="L" />
-        <LetterCell letter="L" />
-        <LetterCell letter="O" number="6" />
+        <LetterCell letter={dataAnswer[1]} number="5" />
+        <LetterCell letter={dataAnswer[1]} number="6" />
+        <LetterCell letter={dataAnswer[1]} number="7" />
+        <LetterCell letter={dataAnswer[1]} number="8" />
+        <LetterCell letter={dataAnswer[1]} number="9" />
         <BlankCell />
         <BlankCell />
-        <LetterCell letter="W" number="7" />
-        <LetterCell letter="O" />
-        <LetterCell letter="R" />
-        <LetterCell letter="L" />
-        <LetterCell letter="D" number="8" />
+        <LetterCell letter={dataAnswer[2]} number="10" />
+        <LetterCell letter={dataAnswer[2]} number="11" />
+        <LetterCell letter={dataAnswer[2]} number="12" />
+        <LetterCell letter={dataAnswer[2]} number="13" />
+        <LetterCell letter={dataAnswer[2]} number="14" />
         <BlankCell />
         <BlankCell />
-        <LetterCell number="9" />
-        <LetterCell />
-        <LetterCell />
-        <LetterCell />
+        <LetterCell letter={dataAnswer[3]} number="15" />
+        <LetterCell letter={dataAnswer[3]} number="16" />
+        <LetterCell letter={dataAnswer[3]} number="17" />
+        <LetterCell letter={dataAnswer[3]} number="18" />
       </div>
     </>
   );
