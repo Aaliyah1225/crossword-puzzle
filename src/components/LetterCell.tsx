501ea1e3323d039d433eca75a2a1a243ca8cb3cf
Aaliyah <aaliyah_haynes@outlook.com>
2025-06-03 15:57:18 -0400

    New Changes

--- src/components/LetterCell.tsx
+++ src/components/LetterCell.tsx
@@ -3,7 +3,7 @@ import cellStyles from "./Cell.module.css";
 function LetterCell(props: any) {
   return (
     <>
-      <div className={cellStyles.cell}>
+      <div tabIndex={0} className={cellStyles.cell}>
         <span className={`${styles.number}`}>{props.number}</span>
         <div className={`${styles.letter}`}>{props.letter}</div>
       </div>
